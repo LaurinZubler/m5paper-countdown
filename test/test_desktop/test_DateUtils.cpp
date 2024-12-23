9580501f76674caa5841eb386b9f39691908f773
Laurin <la.urin@hotmail.ch>
2024-12-23 10:43:24 +0100

    more dateutils tests

--- test/test_desktop/test_DateUtils.cpp
+++ test/test_desktop/test_DateUtils.cpp
@@ -1,17 +1,36 @@
 #include <gtest/gtest.h>
 #include <DateUtils.h>
 
-// Test for getDaysBetween
 TEST(DateUtils, getDaysBetween) {
     tm date1 = {0};
     tm date2 = {0};
 
-    // Set date1 to Jan 1, 2024
+    // 2024-01-01
     date1.tm_year = 2024 - 1900;
     date1.tm_mon = 0;
     date1.tm_mday = 1;
 
-    // Set date2 to Jan 10, 2024
+    // 2024-01-10
+    date2.tm_year = 2024 - 1900;
+    date2.tm_mon = 0;
+    date2.tm_mday = 10;
+
+    int days = DateUtils::getDaysBetween(date1, date2);
+    ASSERT_EQ(days, 9);
+}
+
+TEST(DateUtils, getDaysBetween_withTime) {
+    tm date1 = {0};
+    tm date2 = {0};
+
+    // 2024-01-01 12:15
+    date1.tm_year = 2024 - 1900;
+    date1.tm_mon = 0;
+    date1.tm_mday = 1;
+    date1.tm_hour = 11;
+    date1.tm_min = 14;
+
+    // 2024-01-10 00:00
     date2.tm_year = 2024 - 1900;
     date2.tm_mon = 0;
     date2.tm_mday = 10;
