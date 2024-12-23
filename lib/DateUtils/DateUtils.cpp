9423b7500f33d7cf1bab7b658330ddc033d97fa8
Laurin <la.urin@hotmail.ch>
2024-12-23 11:55:31 +0100

    fix days between, normalize

--- lib/DateUtils/DateUtils.cpp
+++ lib/DateUtils/DateUtils.cpp
@@ -1,11 +1,13 @@
 #include "DateUtils.h"
-#include <cmath>
 
 int DateUtils::getDaysBetween(const tm& date1, const tm& date2) {
-    const time_t time1 = mktime(const_cast<tm*>(&date1));
-    const time_t time2 = mktime(const_cast<tm*>(&date2));
+    const tm date1AtStartOfDay = {0, 0, 0, date1.tm_mday, date1.tm_mon, date1.tm_year};
+    const tm date2AtStartOfDay = {0, 0, 0, date2.tm_mday, date2.tm_mon, date2.tm_year};
+
+    const time_t time1 = mktime(const_cast<tm*>(&date1AtStartOfDay));
+    const time_t time2 = mktime(const_cast<tm*>(&date2AtStartOfDay));
     const double diff = difftime(time2, time1);
-    const int days = static_cast<int>(std::round(diff / (60 * 60 * 24)));
+    const int days = static_cast<int>(diff / (60 * 60 * 24));
     return days;
 }
 
